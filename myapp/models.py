4cd2d1c64a0313a69f41d2a3f18a209865e15413
Uddin Nahim <nahim.211902019@gmail.com>
2025-11-04 00:42:06 +0600

    accepting urls from users

--- myapp/models.py
+++ myapp/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class Link(models.Model):
 
-    def __str__(self):
-        return  self.name
+    
     address = models.CharField(max_length=1000,null=True,blank=True)
     name = models.CharField(max_length=1000,null=True,blank=True)
+
+    def __str__(self):
+        return self.name or self.address
 
